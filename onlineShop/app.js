ea3bea7a93e7bb6a6e40e508a938cc4a84ac5237
AlenS <alenskupnjak@yahoo.com>
2020-07-12 13:24:05 +0200

    002 router

--- onlineShop/app.js
+++ onlineShop/app.js
@@ -1,23 +1,36 @@
-const http = require('http');
-
+// const http = require('http');  R01
+const colors = require('colors')
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 
 // kreiranje express aplikaciju
 const app = express();
 
+// body -parser, bez ovoga ne salje podatke automatski kroz req.body (npm i body-parser)
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use( shopRoutes);
+
 app.use((req, res, next) => {
-    console.log('In the middleware!');
+    console.log('middleware 01!'.yellow);
     next(); // Allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {    
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
+
+
+
+
+app.listen(3200, () => {
+    console.log(`App listening on port 3200!`);
 });
 
-
-// kreiramo server
-const server = http.createServer(app);
-
-server.listen(3200);
+// // kreiramo server  R01
+// const server = http.createServer(app);
+// server.listen(3200);
---
+++ onlineShop/routes/shop.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+
+router.get('/',(req, res, next) => {    
+  console.log('middleware! 03'.blue);
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+
+
+
+module.exports = router
