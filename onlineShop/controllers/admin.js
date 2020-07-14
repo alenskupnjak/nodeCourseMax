5f1419c7e0ebba07d76666254c0a00178894a783
AlenS <alenskupnjak@yahoo.com>
2020-07-14 16:54:36 +0200

    017 edit add page

--- onlineShop/controllers/admin.js
+++ onlineShop/controllers/admin.js
@@ -1,10 +1,12 @@
+const colors = require('colors');
 const Product = require('../models/products');
 
 // /admin/add-product => GET
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Dodaj proizvod',
     path: '/admin/add-product',
+    editing: false
   });
 };
 
@@ -17,10 +19,29 @@ exports.postProduct = (req, res, next) => {
     req.body.description
   );
 
-
   // snimamo dobivene podatke u file
   product.save();
   res.redirect('/');
+};
+
+// /admin/add-product => GET
+exports.getEditProduct = (req, res, next) => {
+  const prodId = req.params.id;
+  console.log(prodId);
+  Product.fetchAll((data) => {
+
+    const podatak = data.find((product) => {
+      return product.id === prodId;
+    });
+    console.log('podatak'.blue, colors.red.underline(podatak));
+
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit proizvod',
+      path: '/admin/edit-product',
+      jedanProduct: podatak,
+      editing: true
+    });
+  });
 };
 
 // /admin/products => GET
--- onlineShop/routes/admin.js
+++ onlineShop/routes/admin.js
@@ -1,6 +1,11 @@
 const path = require('path'); // core nodejs module
 const express = require('express');
-const { getAddProduct, postProduct, getProducts } = require('../controllers/admin');
+const {
+  getAddProduct,
+  postProduct,
+  getProducts,
+  getEditProduct,
+} = require('../controllers/admin');
 
 const router = express.Router();
 
@@ -9,11 +14,13 @@ const router = express.Router();
 router.get('/add-product', getAddProduct);
 
 // /admin/products => GET
-router.get('/products', getProducts );
-
+router.get('/products', getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', postProduct);
+
+// /admin/add-product => GET
+router.get('/edit-product/:id', getEditProduct);
 
 // module.exports = router
 module.exports = router;
